2f8dda1fa1b2dfcb7f8380ee0cdffdf3758d8186
Guillaume Lelarge <guillaume@lelarge.info>
2020-10-28 16:00:06 +0100

    Add translation capacity

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'fr'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -62,3 +62,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+locale_dirs = ['../locales/']
